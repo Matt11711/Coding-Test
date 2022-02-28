878442c8b11161ea6aafe8f9b35392374d3c535d
Matt11711 <matthew.d.allen97@gmail.com>
2022-02-27 21:31:56 -0500

    can now see first page of quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-
-
+var questions = [{question: "Commonly used data types DO NOT include:",answerA: "A: strings", answerB: "B: booleans",answerC: "C: alerts", answerD: "D: numbers"}]
+var questionNumber = 0
 var startPage = function() {
    document.body.innerHTML = '<header>\
        <button type="button" class="btn" id="highscoresButton">Highscores</button>\
@@ -8,9 +8,35 @@ var startPage = function() {
            <h1>Coding Quiz Challenge</h1>\
            <p>Try to answer the following code-related questions within the time limt. Keep in mind that incorrect answers will penalize your scores/time by ten seconds!</p>\
            <button type="button" class="btn" id="startButton">Start Quiz</button>\
-       </main> '
+       </main>\
+       <script src="./assets/js/script.js"> </script>'
+}
+document.onload = startPage()
+var generateQuiz = function() {
+    document.querySelector("main").innerHTML='<h1 id="question">Questions Here</h1>\
+    <button type="button" class="btn" id="answer-A">Answer Here</button>\
+    <button type="button" class="btn" id="answer-B">Answer Here</button>\
+    <button type="button" class="btn" id="answer-C">Answer Here</button>\
+    <button type="button" class="btn" id="answer-D">Answer Here</button>';
+    nextQuestion()
+}
+var nextQuestion = function() {
+// if (questionNumber<(questions.length-1)) {
+    document.querySelector("#question").textContent = questions[questionNumber].question
+    document.querySelector("#answer-A").textContent = questions[questionNumber].answerA
+    document.querySelector("#answer-B").textContent = questions[questionNumber].answerB
+    document.querySelector("#answer-C").textContent = questions[questionNumber].answerC
+    document.querySelector("#answer-D").textContent = questions[questionNumber].answerD
+// }
+
+}
+var buttonHandler = function(event) {
+var targetEl = event.target;
+if (targetEl.matches("#startButton")) {
+    generateQuiz();
 }
 
-
-document.onload = startPage()
-
+}
+var header = document.querySelector("header");
+var main = document.querySelector("main")
+main.addEventListener("click",buttonHandler)
